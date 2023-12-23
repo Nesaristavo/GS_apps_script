4752cf3fe930f98024b2bbf2da0591826b17cb75
Fung Kwong <119128258+Nesaristavo@users.noreply.github.com>
2023-12-23 19:30:42 +0300

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,13 @@
+function getLastEditAuthor() {
+   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+   var range = sheet.getRange('V5'); // Replace 'A1' with your cell address
+   var lastEdit = range.getA1Notation();
+   Logger.log('Last change in cell ' + lastEdit);
+   var lastEditor = range.getRichTextValue().getLinkUrl(); // Get the ID of the author of the last change
+
+   if (lastEditor) {
+     Logger.log('The last change in the cell ' + lastEdit + ' was made by user ID: ' + lastEditor);
+   } else {
+     Logger.log('Last change author ID not found');
+   }
+}
